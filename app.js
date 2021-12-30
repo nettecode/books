85d05ec1d447528a6ef1bfca020bbad386bf0313
Żaneta Jażdżyk <zaneta.jazdzyk@gmail.com>
2021-12-30 15:50:51 +0100

    html > hbs & displaying books

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const path = require('path');
+const expHbs = require('express-handlebars');
+
+const books = [{
+  title: 'Harry Potter And The Prisoner Of Azkaban'
+}];
 
 const app = express();
+app.engine('hbs', expHbs());
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 app.use('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index', {
+    pageTitle: 'List of Books',
+    books: books,
+    hasBooks: books.length > 0
+  });
 });
 
 app.listen(3000);
